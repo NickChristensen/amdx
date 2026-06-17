a22a739b9de91724571aa021f7d7d8b4768cea02
Nick Christensen <nick.g.christensen@gmail.com>
2026-06-16 23:38:28 -0500

    Allow tailscale hostname

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["macmini.pony-rattlesnake.ts.net"],
+};
 
 export default nextConfig;
